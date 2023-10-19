74c9b1ee8cc320a052288e40340c2f26f9e34031
Jeremi Joslin <jeremi@newlogic.com>
2023-10-19 08:43:00 +0200

    WIP dev

--- spp_commcare/controllers/commcare_form.py
+++ spp_commcare/controllers/commcare_form.py
@@ -6,12 +6,13 @@ print('Hello world! CommCareController')
 
 class CommCareController(http.Controller):
 
-    @http.route('/commcare/form/', methods=['GET'], type='http', auth='api_key')
+    # @http.route('/commcare/form/', methods=['GET'], type='http', auth='api_key')
+    @http.route('/commcare/form/', methods=['GET'], type='http', auth='public')
     def get_sample(self, **kwargs):
         print('Hello world! CommCareController get_sample')
         return json.dumps({'success': True, 'form_id': 1})
 
-    @http.route('/commcare/form/', auth='public', methods=['POST'], csrf=False, type='http')
+    @http.route('/commcare/form/', auth='api_key', methods=['POST'], csrf=False, type='json')
     def create_form(self, **kwargs):
         print('Hello world! CommCareController create_form')
         CommCareForm = http.request.env['spp.commcare.form']
--- spp_commcare/models/commcare_form.py
+++ spp_commcare/models/commcare_form.py
@@ -3,6 +3,25 @@ from datetime import datetime
 
 from odoo import models, fields, api
 
+"""
+"#type": "data",
+      "@name": "Add Member",
+      "@uiVersion": "1",
+      "@version": "44",
+      "@xmlns": "http://openrosa.org/formdesigner/323346D9-79F7-4BFC-8CF3-E21309E64404",
+"""
+
+RESERVED_PROPERTY_NAME = [
+    "subcase_0",
+    "meta",
+    "case",
+    "@xmlns",
+    "@version",
+    "@uiVersion",
+    "@name",
+    "#type",
+    "_form_type",
+]
 
 class CommCareForm(models.Model):
     _name = 'spp.commcare.form'
@@ -42,11 +61,12 @@ class CommCareForm(models.Model):
             print(form.form_name)
             if form.form_name == 'Register New Group':
                 is_group = True
-            elif form.form_name != 'Register New Member':
+            elif form.form_name != 'Add New Member':
                 continue  # Skip to the next iteration if form_name doesn't match
 
             # Set the is_group flag
             partner_data['is_group'] = is_group
+            partner_data['is_registrant'] = True
 
             # Extract properties from stored JSON
             properties_json = form.properties
@@ -54,15 +74,123 @@ class CommCareForm(models.Model):
 
             # Compare JSON keys with res.partner fields and map
             for key, value in properties.items():
+                if key in RESERVED_PROPERTY_NAME:
+                    continue
+                #TODO: Fix dirty hardcod fix
+                if key == 'first_name':
+                    key = "given_name"
+                elif key == 'last_name':
+                    key = "family_name"
+                elif key == 'gender' and value is not None:
+                    value = value[0].upper() + value[1:].lower()
+
                 if key in partner_fields:
                     partner_data[key] = value
 
             print(partner_data)
+            # TODO: FIX! find why it is not recomputed
+            partner_data['name'] = "plop"
             # Create new res.partner record with mapped fields
             if partner_data:  # Only create if there's data to insert
-                self.env['res.partner'].create(partner_data)
+                partner = self.env['res.partner'].create(partner_data)
+                self._associate_case_with_partner(form, partner)
+                if not is_group:
+                    self._associate_individual_with_group(form, partner)
             else:
                 print('No partner data found')
+
+    @api.model
+    def create_event_data_from_form(self):
+        print("create_event_data_from_form")
+        # Loop over the recordset
+        for form in self:
+            # Extract properties from stored JSON
+            properties_json = form.properties
+            properties = json.loads(properties_json)
+            if not '_form_type' in properties or not properties['_form_type'].startswith("spp.event."):
+                # Not an event_data form type
+                continue
+            form_type = properties['_form_type']
+            form_fields = self.env[form_type]._fields.keys()
+
+            related_case = self._get_related_case(properties)
+            if not (related_case.id and related_case.partner_id):
+                print("no related case or partner for form: %s" % form.id)
+                continue
+
+            form_data = {}
+
+            for key, value in properties.items():
+                if key in RESERVED_PROPERTY_NAME:
+                    continue
+                if key in form_fields:
+                    form_data[key] = value
+                else:
+                    print("Missing field: %s" % key)
+
+            form_id = self.env[form_type].create(form_data)
+            vals_event_data = {
+                "partner_id": related_case.partner_id.id,
+                "model": form_type,
+                "res_id": form_id.id,
+            }
+            event_data = self.env["spp.event.data"].create(vals_event_data)
+
+    def _get_related_case(self, properties):
+        if 'subcase_0' in properties.keys():
+            # This is used for individuals
+            case_data = properties['subcase_0']["case"]
+        else:
+            # This is for groups
+            case_data = properties["case"]
+        case_id = case_data['@case_id']
+        existing_case = self.env['spp.commcare.case'].search([('case_id', '=', case_id)],
+                                                             order='create_date desc', limit=1)
+        return existing_case
+
+    def _associate_individual_with_group(self, form, individual):
+        # TODO: Check of the group membership app is installed
+        properties = json.loads(form.properties)
+
+        group_case = self._get_related_case(properties)
+        if group_case and group_case.partner_id:
+            self.env["g2p.group.membership"].create({
+                'group': group_case.partner_id.id,
+                'individual': individual.id
+            })
+
+    def _associate_case_with_partner(self, form, partner):
+        domain = form.domain
+        properties = json.loads(form.properties)
+
+        if 'subcase_0' in properties.keys():
+            # This is used for individuals
+            case_data = properties['subcase_0']["case"]
+        else:
+            # This is for groups
+            case_data = properties["case"]
+        case_id = case_data['@case_id']
+        existing_case = self.env['spp.commcare.case'].search([('case_id', '=', case_id)],
+                                                             order='create_date desc', limit=1)
+        print(existing_case)
+        if not existing_case:
+            case_data = {
+                'case_id': case_id,
+                'domain': domain,
+                'user_id': case_data["@user_id"],
+                'is_partial': True,
+                'partner_id': partner.id
+                # Add any other default or inferred fields for parent case here
+            }
+            case = self.env['spp.commcare.case'].create(case_data)
+        else:
+            print("PartnerId %s" % existing_case.partner_id)
+            if not existing_case.partner_id.id:
+                existing_case.write({
+                    "partner_id": partner.id
+                })
+            elif existing_case.partner_id.id != partner.id:
+                raise Exception("Unexpected existing_case.partner_id != partner.id")
 
     def _convert_commcare_date(self, date_str):
         if not date_str:
@@ -82,6 +210,10 @@ class CommCareForm(models.Model):
             case = self.env['spp.commcare.case'].search([('case_id', '=', primary_case_id)],
                                                     order='create_date desc',
                                                     limit=1)
+            if case:
+                case = case.id
+            else:
+                case = None
         else:
             case = None
 
@@ -114,7 +246,7 @@ class CommCareForm(models.Model):
 
         # Create the form and return
         new_form = self.sudo().env['spp.commcare.form'].create(form_data)
-        new_form.create_partner_from_form()
+        # new_form.create_partner_from_form()
         return new_form
 
 
